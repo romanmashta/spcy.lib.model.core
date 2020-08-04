1aa9043a227a11e8194bc07a176e1f6350fd4d0d
Roman Mashta <roman.mashta@gmail.com>
2020-08-04 22:24:49 +0300

    Ignore assertion for schemas

--- src/store/index.schema.ts
+++ src/store/index.schema.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import * as r from '@spcy/lib.core.reflection';
 import { CollectionModule, Types as CollectionTypes } from './collection.schema';
 import { QueryModule, Types as QueryTypes } from './query.schema';
